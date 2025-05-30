f33bd071104dd5a37705086d5720223b6d91834b
Laura <laura.aguiar3@etec.sp.gov.br>
2025-05-30 11:42:35 -0300

    Aula 30-05-2025

--- src/controllers/sensor.js
+++ src/controllers/sensor.js
@@ -108,9 +108,27 @@ module.exports = {
     }, 
     async apagarSensor(request, response) {
         try {
+
+            const { id } = request.params;
+
+            const sql = `DELETE FROM sensor WHERE tipo_sensor = ?`;
+
+            const values = [id];
+
+            const [result] = await db.query(sql, values);
+
+            
+            if (result.affectedRows === 0) {
+                return response.status(404).json({
+                    sucesso:false,
+                    mensagem: `Sensor ${id} não encontrado!`,
+                    dados: null
+                });
+            } 
+
             return response.status(200).json({
                 sucesso: true, 
-                mensagem: 'Exclusão de sensor', 
+                mensagem: `Sensor ${id} excluido com sucesso`,
                 dados: null
             });
         } catch (error) {
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,7 +6,7 @@ const SensorController = require('../controllers/sensor');
 router.get('/sensor', SensorController.listarSensor); 
 router.post('/sensor', SensorController.cadastrarSensor); 
 router.patch('/sensor/:id', SensorController.editarSensor); 
-router.delete('/sensor', SensorController.apagarSensor); 
+router.delete('/sensor/:id', SensorController.apagarSensor); 
 
 
 module.exports = router;
